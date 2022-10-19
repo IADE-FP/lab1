0dc284b2ce362e69e756eeb04e6b1dd725f6cfb3
amgs <amgs@amgs.localdomain>
2022-10-19 17:18:39 +0100

    Started with part 1.

---
+++ parte1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // (a) `olá mundo`
+    printf("olá mundo\n");
+
+    // (b) `"olá mundo"`
+    printf("\"olá mundo\"\n");
+
+    // (c) $4 \times 4$
+
+    // (d) $3 \times 1000$
+
+    // (e) $3000000 \times 9999999$
+
+    // (f) $\sqrt{177}$
+
+    // (g) $\frac{20}{3}$
+
+    // (h) $7^2$
+
+    // (i) $\sum^{5}_{i=0}n_i, \forall n \in [1,1,2,3,5]$
+    return 0;
+}
